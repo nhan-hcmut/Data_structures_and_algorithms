9c223262a6cf5a02b14a12e73f452c2aa0c5bdd9
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-02-03 07:56:00 +0700

    Update CantorSet.java

--- Chap_3_Recursion/Cantor_set/CantorSet.java
+++ Chap_3_Recursion/Cantor_set/CantorSet.java
@@ -1,22 +1,32 @@
-import java.util.Scanner;
+/*
+Print Cantor set by characters X and spaces
+
+E.g. a Cantor set with order = 3
+
+XXXXXXXXX => Line = 0. Stop = 9. There are 9 Xs. 9 = 3^2 = power(order-1-line)
+XXX   XXX => Line = 1. Stop = 3. There are 3 Xs. 3 = 3^1 = power(order-1-line)
+X X   X X => Line = 2. Stop = 1. There is 1 X. 1 = 3^0 = power(order-1-line)
+*/
+
+import java.util.Scanner; // Library to take input from users
 
 public class CantorSet {
-    static int stop = 0;
+    static int stop = 0; // A flag to stop the recursion
     
-    public static int power(int n) {
+    public static int power(int n) { // Calculate 3 raised to power of n
         
         if (n == 0) return 1;
         
         return 3 * power(n-1);
     }
     
-    public static void print_space(int n) {
+    public static void print_space(int n) { // Print n spaces
         for (int i=0; i<n; i++) {
             System.out.print(" ");
         }
     }
     
-    public static void print_x(int n) {
+    public static void print_x(int n) { // Print X(s) recursively
         if (n == stop) {
             for (int i=0; i<n; i++) {
                 System.out.print("X");
