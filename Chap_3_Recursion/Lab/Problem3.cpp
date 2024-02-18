1a0066db33efec016abcd287af22b016d96b96e5
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-02-18 20:10:34 +0700

    Create Problem3.cpp

---
+++ Chap_3_Recursion/Lab/Problem3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int findMax(int* arr, int length) {
+    if (length == 1) return arr[0];
+    
+    return ( arr[0] > findMax(arr+1, length-1) ) ? arr[0] : findMax(arr+1, length-1);
+}
+
+int main() {
+    int arr[] = {10, 5, 7, 9, 15, 6, 11, 8, 12, 2};
+    cout << findMax(arr, 10);
+
+    return 0;
+}
