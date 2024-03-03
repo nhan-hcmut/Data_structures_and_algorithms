03fefba1967f6d96a7b86bdcbbb4500510c8758a
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-03-03 15:47:18 +0700

    Create Problem9.cpp

---
+++ Chap_3_Recursion/Lab/Problem9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int strLen(char* str) {
+    
+    if (*str == '\0') return 0;
+    
+    return 1 + strLen(str+1);
+}
+
+int main()
+{
+    char str[] = "Ho Chi Minh City University of Technology";
+    
+    cout << strLen(str);
+
+    return 0;
+}
