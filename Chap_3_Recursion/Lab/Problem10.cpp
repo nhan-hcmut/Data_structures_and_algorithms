420052a255c6bf66a4ef53f93bd39366e6be6438
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-03-17 08:51:13 +0700

    Create Problem10.cpp

---
+++ Chap_3_Recursion/Lab/Problem10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Helping functions
+string findFirstWord(string str, int len, int index = 0) {
+
+    if (index >= len) return "";
+
+    if (str[index] != ' ') return str[index] + findFirstWord(str, len, index+1);
+
+    return "";
+}
+// End helping functions
+
+string reverseSentence(string s) {
+
+    string firstWord = findFirstWord(s, s.length());
+
+    if (firstWord.length()+1 >= s.length()) return firstWord;
+    
+    return reverseSentence(s.substr(firstWord.length()+1, s.length()-firstWord.length()-1)) + " " + firstWord;
+}
+
+int main() {
+
+    cout << reverseSentence("data structures and algorithms is challenging");
+
+    return 0;
+}
