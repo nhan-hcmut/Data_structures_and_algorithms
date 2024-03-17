8ca4d6e42617be574ca754d277ef23cd70cd7125
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-03-17 20:17:21 +0700

    Update Problem3.cpp

--- Chap_3_Recursion/Lab/Problem3.cpp
+++ Chap_3_Recursion/Lab/Problem3.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int findMore(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
 int findMax(int* arr, int length) {
-    if (length == 1) return arr[0];
     
-    return ( arr[0] > findMax(arr+1, length-1) ) ? arr[0] : findMax(arr+1, length-1);
+    if (length == 1) return *arr;
+    
+    return findMore(arr[length-1], findMax(arr, length-1));
 }
 
 int main() {
+    
     int arr[] = {10, 5, 7, 9, 15, 6, 11, 8, 12, 2};
+    
     cout << findMax(arr, 10);
 
     return 0;
