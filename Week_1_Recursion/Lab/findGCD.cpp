ead851bdf76635ddba999e5c56ccab963dd01a75
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-07-10 17:29:56 +0700

    Update findGCD.cpp

--- Week_1_Recursion/Lab/findGCD.cpp
+++ Week_1_Recursion/Lab/findGCD.cpp
@@ -7,6 +7,15 @@ int findGCD(int a, int b) {
 }
 
 int main() {
-    cout << findGCD(124, 32);
+    cout << findGCD(124, 32) << endl; // result: 4
+
+    cout << findGCD(138, 184) << endl; // result: 46
+
+    cout << findGCD(138, 91) << endl; // result: 1
+
+    cout << findGCD(138, 6) << endl; // result: 6
+
+    cout << findGCD(307, 2000) << endl; // result: 1
+
     return 0;
 }
