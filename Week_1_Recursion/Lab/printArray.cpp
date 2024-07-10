f6f0d387697499baeb43c82d3545e2bed8ed06c8
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-07-10 16:45:58 +0700

    Update printArray.cpp

--- Week_1_Recursion/Lab/printArray.cpp
+++ Week_1_Recursion/Lab/printArray.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 void printArray(int n) {
     if (n == 0) cout << "0";
-
     else {
         printArray(n-1);
-        cout << ", ";
-        cout << n;
+        cout << ", " << n;
     }
 }
 
 int main() {
     printArray(5);
     cout << endl;
+    
     printArray(10);
-
+    cout << endl;
     return 0;
 }
