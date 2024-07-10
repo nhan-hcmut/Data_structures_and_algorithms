9944adfbb9185a78882eb2079eaec6508b829e5d
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-07-10 21:59:25 +0700

    Update myArrayToInt.cpp

--- Week_1_Recursion/Lab/myArrayToInt.cpp
+++ Week_1_Recursion/Lab/myArrayToInt.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int myArrayToInt(char* str, int n) {
     
-    if (n == 1) return (int)str[0] - 48; // ASCII code of character '0' is 48
+    if (n == 1) return (int)(*str - '0');
     
-    return ((int)str[n-1]-48) + (10*myArrayToInt(str, n-1));
+    return (int)(str[n-1] - '0') + (10 * myArrayToInt(str, n-1));
 }
 
 int main()
