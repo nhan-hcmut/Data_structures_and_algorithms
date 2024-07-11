226a37b23a706f1e7c68554ce7ac5ebc0fcc6b45
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-07-11 10:39:53 +0700

    Create Point.cpp

---
+++ Week_2_Array_list_and_Singly_linked_list/OOP_review/Point.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Point {
+
+    private:
+    double x, y;
+
+    public:
+    Point() {
+        /*
+         * STUDENT ANSWER
+        * TODO: set zero x-y coordinate
+        */
+        this->x = 0;
+        this->y = 0;
+    }
+
+    Point(double x, double y) {
+        /*
+         * STUDENT ANSWER
+        */
+        this->x = x;
+        this->y = y;
+    }
+
+    void setX(double x) {
+        /*
+         * STUDENT ANSWER
+        */
+        this->x = x;
+    }
+
+    void setY(double y) {
+        /*
+         * STUDENT ANSWER
+        */
+        this->y = y;
+    }
+
+    double getX() const {
+        /*
+         * STUDENT ANSWER
+        */
+        return this->x;
+    }
+
+    double getY() const {
+        /*
+         * STUDENT ANSWER
+        */
+        return this->y;
+    }
+
+    double distanceToPoint(const Point& pointA) {
+        /*
+         * STUDENT ANSWER
+         * TODO: calculate the distance from this point to point A in the coordinate plane
+        */
+        return sqrt(pow(this->x - pointA.getX(), 2) + pow(this->y - pointA.getY(), 2));
+    }
+};
+
+int main() {
+
+    // Test 1
+    Point A(2, 3);
+    cout << A.getX() << " " << A.getY(); // Result: 2 3
+    
+    // Test 2
+    Point A(2, 3);
+    Point B(1, 1);
+    cout << pow(A.distanceToPoint(B), 2); // Result: 5
+    
+    return 0;
+}
