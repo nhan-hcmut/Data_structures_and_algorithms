2552ea2baf184e831b3345f1abf9eab85ceee821
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-07-11 13:56:53 +0700

    Create buyCar.cpp

---
+++ Week_2_Array_list_and_Singly_linked_list/Array_list/buyCar.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int buyCar(int* nums, int length, int k) {
+    int sum = 0, count = 0;
+    sort(nums, nums + length);
+    for (short i = 0; i < length; i++) {
+        
+        if ((sum + nums[i]) > k) return count;
+        
+        sum += nums[i];
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    
+    int nums[] = {90, 30, 40, 90, 20};
+
+    int length = sizeof(nums) / sizeof(nums[0]);
+    
+    cout << buyCar(nums, length, 90) << "\n"; // Result: 3
+    
+    return 0;
+}
