2233c5edd1adec20b5d63690313143180ea3f6ca
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2024-08-22 22:55:12 +0700

    Update getHeight_preOrder_inOrder_postOrder.cpp

--- Week_5_Basic_tree/getHeight_preOrder_inOrder_postOrder.cpp
+++ Week_5_Basic_tree/getHeight_preOrder_inOrder_postOrder.cpp
@@ -130,22 +130,162 @@ string BinaryTree<K, V>::postOrder() {
 // STUDENT ANSWER END
 
 int main() {
-
+    // test 1
     BinaryTree<int, int> binaryTree;
     binaryTree.addNode("", 2, 4); // Add to root
     binaryTree.addNode("L", 3, 6); // Add to root's left node
     binaryTree.addNode("R", 5, 9); // Add to root's right node
 
-    cout << binaryTree.getHeight() << endl;
-    cout << binaryTree.preOrder() << endl;
-    cout << binaryTree.inOrder() << endl;
-    cout << binaryTree.postOrder() << endl;
+    cout << binaryTree.getHeight() << endl; // result: 2
+    cout << binaryTree.preOrder() << endl; // result: 4 6 9
+    cout << binaryTree.inOrder() << endl; // result: 6 4 9
+    cout << binaryTree.postOrder() << endl; // result: 6 9 4
     
-    /* result:
-    2
-    4 6 9
-    6 4 9
-    6 9 4
+    /* test 2
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("", 2, 4);
+
+    cout << binaryTree.getHeight() << endl; // result: 1
+    cout << binaryTree.preOrder() << endl; // result: 4
+    cout << binaryTree.inOrder() << endl; // result: 4
+    cout << binaryTree.postOrder() << endl; // result: 4
+    */
+    
+    /* test 3
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("", 2, 4);
+    binaryTree.addNode("L", 3, 6);
+    binaryTree.addNode("R", 5, 9);
+    binaryTree.addNode("LL", 4, 10);
+    binaryTree.addNode("LR", 6, 2);
+
+    cout << binaryTree.getHeight() << endl; // result: 3
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 9
+    cout << binaryTree.inOrder() << endl; // result: 10 6 2 4 9
+    cout << binaryTree.postOrder() << endl; // result: 10 2 6 9 4
+    */
+
+    /* test 4
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("", 2, 4);
+    binaryTree.addNode("L", 3, 6);
+    binaryTree.addNode("R", 5, 9);
+    binaryTree.addNode("LL", 4, 10);
+    binaryTree.addNode("RL", 6, 2);
+
+    cout << binaryTree.getHeight() << endl; // result: 3
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 9 2
+    cout << binaryTree.inOrder() << endl; // result: 10 6 4 2 9
+    cout << binaryTree.postOrder() << endl; // result: 10 6 2 9 4
+    */
+
+    /* test 5
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LLL",6, 2);
+    binaryTree.addNode("LLLR",7, 7);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 9
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 4 9
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 6 9 4
+    */
+
+    /* test 6
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LLL",6, 2);
+    binaryTree.addNode("LLLR",7, 7);
+    binaryTree.addNode("RR",8, 30);
+    binaryTree.addNode("RL",9, 307);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 9 307 30
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 4 307 9 30
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 6 307 30 9 4
+    */
+
+    /* test 7
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LR",6, -3);
+    binaryTree.addNode("LLL",7, 2);
+    binaryTree.addNode("LLLR",8, 7);
+    binaryTree.addNode("RR",9, 30);
+    binaryTree.addNode("RL",10, 307);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 -3 9 307 30
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 -3 4 307 9 30
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 -3 6 307 30 9 4
+    */
+
+    /* test 8
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LR",6, -3);
+    binaryTree.addNode("LLL",7, 2);
+    binaryTree.addNode("LLLR",8, 7);
+    binaryTree.addNode("RR",9, 30);
+    binaryTree.addNode("RL",10, 307);
+    binaryTree.addNode("RLL",11, 2000);
+    binaryTree.addNode("RLR",12, 2000);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 -3 9 307 2000 2000 30
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 -3 4 2000 307 2000 9 30
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 -3 6 2000 2000 307 30 9 4
+    */
+
+    /* test 9
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LR",6, -3);
+    binaryTree.addNode("LLL",7, 2);
+    binaryTree.addNode("LLLR",8, 7);
+    binaryTree.addNode("RR",9, 30);
+    binaryTree.addNode("RL",10, 307);
+    binaryTree.addNode("RLL",11, 2000);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 -3 9 307 2000 30
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 -3 4 2000 307 9 30
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 -3 6 2000 307 30 9 4
+    */
+
+    /* test 10
+    BinaryTree<int, int> binaryTree;
+    binaryTree.addNode("",2, 4);
+    binaryTree.addNode("L",3, 6);
+    binaryTree.addNode("R",5, 9);
+    binaryTree.addNode("LL",4, 10);
+    binaryTree.addNode("LR",6, -3);
+    binaryTree.addNode("LLL",7, 2);
+    binaryTree.addNode("LLLR",8, 7);
+    binaryTree.addNode("RR",9, 30);
+    binaryTree.addNode("RL",10, 307);
+    binaryTree.addNode("RLL",11, 2000);
+    binaryTree.addNode("RLLL",11, 2000);
+
+    cout << binaryTree.getHeight() << endl; // result: 5
+    cout << binaryTree.preOrder() << endl; // result: 4 6 10 2 7 -3 9 307 2000 2000 30
+    cout << binaryTree.inOrder() << endl; // result: 2 7 10 6 -3 4 2000 2000 307 9 30
+    cout << binaryTree.postOrder() << endl; // result: 7 2 10 -3 6 2000 2000 307 30 9 4
     */
     return 0;
 }
