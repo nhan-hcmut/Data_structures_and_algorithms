29a24affad2c5b11f367a06934315d8102dc38c3
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 20:27:12 +0700

    Update CantorSet.c

--- Week_1_Recursion/Cantor_set/CantorSet.c
+++ Week_1_Recursion/Cantor_set/CantorSet.c
@@ -3,9 +3,9 @@ Print Cantor set by X(s) and space(s)
 
 E.g. a Cantor set with order = 3
 
-XXXXXXXXX => Line = 0. Stop = 9. There are 9 Xs. 9 = 3^2 = power(order-1-line)
-XXX   XXX => Line = 1. Stop = 3. There are 3 Xs. 3 = 3^1 = power(order-1-line)
-X X   X X => Line = 2. Stop = 1. There is 1 X. 1 = 3^0 = power(order-1-line)
+XXXXXXXXX => line = 0, stop = 9. There are 9 Xs. 9 = 3^2 = three_power(order - 1 - line)
+XXX   XXX => line = 1, stop = 3. There are 3 Xs. 3 = 3^1 = three_power(order - 1 - line)
+X X   X X => line = 2, stop = 1. There is 1 X. 1 = 3^0 = three_power(order - 1 - line)
 */
 #include<stdio.h>
 
