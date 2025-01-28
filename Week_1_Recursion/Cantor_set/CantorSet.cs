e409f4a7c8a9feec9a390491cc28ef3620425628
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 20:39:54 +0700

    Update CantorSet.cs

--- Week_1_Recursion/Cantor_set/CantorSet.cs
+++ Week_1_Recursion/Cantor_set/CantorSet.cs
@@ -3,9 +3,9 @@ Print Cantor set by X(s) and spaces.
 
 E.g. a Cantor set with order = 3
 
-XXXXXXXXX => Line = 0. Stop = 9. There are 9 Xs. 9 = 3^2 = power(order-1-line)
-XXX   XXX => Line = 1. Stop = 3. There are 3 Xs. 3 = 3^1 = power(order-1-line)
-X X   X X => Line = 2. Stop = 1. There is 1 X. 1 = 3^0 = power(order-1-line)
+XXXXXXXXX => line = 0, stop = 9. There are 9 Xs. 9 = 3^2 = three_power(order - 1 - line)
+XXX   XXX => line = 1, stop = 3. There are 3 Xs. 3 = 3^1 = three_power(order - 1 - line)
+X X   X X => line = 2, stop = 1. There is 1 X. 1 = 3^0 = three_power(order - 1 - line)
 */
 using System;
 
@@ -13,10 +13,10 @@ public class Cantor
 {
     public static int stop = 0; // A flag to stop the recursion
 
-    public static int power(int n) // 3 raised to the power of n
+    public static int three_power(int n) // 3 raised to the power of n
     {
         if (n == 0) return 1;
-        return 3 * power(n - 1);
+        return 3 * three_power(n - 1);
     }
 
     public static void print_space(int n) { // Print n spaces
@@ -46,14 +46,15 @@ public class Cantor
     {
         int order = 0; // Order of Cantor set
 
-        while (order<1 || 5<order) {
+        while (order < 1 || 5 < order) {
             Console.Write("Order (from 1 to 5): ");
             order = Convert.ToInt32(Console.ReadLine());
         }
 
         for (int line = 0; line < order; line++) {
-            stop = power(order - 1 - line);
-            print_x(power(order - 1));
+            stop = three_power(order - 1 - line);
+            
+            print_x(three_power(order - 1));
             Console.WriteLine();
         }
         Console.ReadLine();
