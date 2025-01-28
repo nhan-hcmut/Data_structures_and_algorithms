757296de67e8724397b672ddea1e1248a017146e
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 20:49:18 +0700

    Update CantorSet.java

--- Week_1_Recursion/Cantor_set/CantorSet.java
+++ Week_1_Recursion/Cantor_set/CantorSet.java
@@ -3,9 +3,9 @@ Print Cantor set by characters X and spaces
 
 E.g. a Cantor set with order = 3
 
-XXXXXXXXX => Line = 0. Stop = 9. There are 9 Xs. 9 = 3^2 = power(order-1-line)
-XXX   XXX => Line = 1. Stop = 3. There are 3 Xs. 3 = 3^1 = power(order-1-line)
-X X   X X => Line = 2. Stop = 1. There is 1 X. 1 = 3^0 = power(order-1-line)
+XXXXXXXXX => line = 0, stop = 9. There are 9 Xs. 9 = 3^2 = three_power(order - 1 - line)
+XXX   XXX => line = 1, stop = 3. There are 3 Xs. 3 = 3^1 = three_power(order - 1 - line)
+X X   X X => line = 2, stop = 1. There is 1 X. 1 = 3^0 = three_power(order - 1 - line)
 */
 
 import java.util.Scanner; // Library to take input from users
@@ -13,30 +13,30 @@ import java.util.Scanner; // Library to take input from users
 public class CantorSet {
     static int stop = 0; // A flag to stop the recursion
     
-    public static int power(int n) { // Calculate 3 raised to power of n
+    public static int three_power(int n) { // Calculate 3 raised to power of n
         
         if (n == 0) return 1;
         
-        return 3 * power(n-1);
+        return 3 * three_power(n - 1);
     }
     
     public static void print_space(int n) { // Print n spaces
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print(" ");
         }
     }
     
     public static void print_x(int n) { // Print X(s) recursively
         if (n == stop) {
-            for (int i=0; i<n; i++) {
+            for (int i = 0; i < n; i++) {
                 System.out.print("X");
             }
         }
         
         else {
-            print_x(n/3);
-            print_space(n/3);
-            print_x(n/3);
+            print_x(n / 3);
+            print_space(n / 3);
+            print_x(n / 3);
         }
     }
     
@@ -46,14 +46,15 @@ public class CantorSet {
 	    
 	    Scanner input = new Scanner(System.in);
 	    
-	    while (order < 1) {
-	        System.out.print("Order (>0): ");
+	    while (order < 1 || order > 5) {
+	        System.out.print("Order (from 1 to 5): ");
 	        order = input.nextInt();
 	    }
 	    
 	    for (int line = 0; line < order; line++) {
-	        stop = power(order-1-line);
-	        print_x(power(order-1));
+	        stop = three_power(order - 1 - line);
+            
+	        print_x(three_power(order - 1));
 	        System.out.print("\n");
 	    }
 	}
