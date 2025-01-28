c6a5a8ff1130ae2bb6c72ef142e52348d4b8acf3
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 20:22:40 +0700

    Update CantorSet.c

--- Week_1_Recursion/Cantor_set/CantorSet.c
+++ Week_1_Recursion/Cantor_set/CantorSet.c
@@ -1,5 +1,5 @@
 /*
-Print Cantor set by X(s) and spaces
+Print Cantor set by X(s) and space(s)
 
 E.g. a Cantor set with order = 3
 
@@ -11,27 +11,27 @@ X X   X X => Line = 2. Stop = 1. There is 1 X. 1 = 3^0 = power(order-1-line)
 
 int stop = 0; // A flag to stop the recursion
 
-int power(int n) { // 3 raised to the power of n
+int three_power(int n) { // 3 raised to the power of n
     if (n == 0) return 1;
-    return 3 * power(n-1);
+    return 3 * three_power(n - 1);
 }
 
-void print_space(int n) { // Just print n spaces
-    for (int i=0; i<n; i++) {
+void print_space(int n) { // Print n spaces
+    for (int i = 0; i < n; i++) {
         printf(" ");
     }
 }
 
 void print_x(int n) { // Print n X(s) recursively
     if (n == stop) {
-        for (int i=0; i<stop; i++) {
+        for (int i = 0; i < stop; i++) {
             printf("X");
         }
     }
     else {
-        print_x(n/3);
-        print_space(n/3);
-        print_x(n/3);
+        print_x(n / 3);
+        print_space(n / 3);
+        print_x(n / 3);
     }
 }
 
@@ -39,15 +39,15 @@ void main() {
 
     int order = 0; // Order of Cantor set
 
-    while (order<1 || 5<order) {
+    while (order < 1 || 5 < order) {
         printf("Order (from 1 to 5): ");
         scanf("%d", &order);
     }
 
-    for (int line=0; line<order; line++) {
-        stop = power(order-1-line);
+    for (int line = 0; line < order; line++) {
+        stop = three_power(order - 1 - line);
 
-        print_x(power(order-1));
+        print_x(three_power(order - 1));
         printf("\n");
     }
 }
