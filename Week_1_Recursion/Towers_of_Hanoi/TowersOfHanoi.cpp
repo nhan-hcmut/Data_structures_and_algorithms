96c5540acfdb3aa46faea1a9dd68dd2e7ff09976
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-06-12 21:30:14 +0700

    Update TowersOfHanoi.cpp

--- Week_1_Recursion/Towers_of_Hanoi/TowersOfHanoi.cpp
+++ Week_1_Recursion/Towers_of_Hanoi/TowersOfHanoi.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
 
-void move(int n, string source, string destination, string auxiliary) {
+void move(int n, const string source, const string destination, const string auxiliary) {
     static int step = 0;
     
     if (n == 1) {
         cout << "Step " << ++step << ": Move a disk from " << source << " to " << destination << endl;
     }
     else {
-        move(n-1, source, auxiliary, destination);
+        move(n - 1, source, auxiliary, destination);
         move(1, source, destination, auxiliary);
-        move(n-1, auxiliary, destination, source);
+        move(n - 1, auxiliary, destination, source);
     }
 }
 
