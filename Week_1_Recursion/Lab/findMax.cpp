76e156a4b30af0d06e374a5e0454f30a5f5fe92d
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-06-22 22:30:58 +0700

    Update findMax.cpp

--- Week_1_Recursion/Lab/findMax.cpp
+++ Week_1_Recursion/Lab/findMax.cpp
@@ -1,15 +1,11 @@
 #include<iostream>
 using namespace std;
 
-int findMore(int num1, int num2) {
-    return (num1 > num2) ? num1 : num2;
-}
-
 int findMax(int* arr, int length) {
     
     if (length == 1) return *arr;
     
-    return findMore(arr[length-1], findMax(arr, length-1));
+    return max(arr[length - 1], findMax(arr, length - 1));
 }
 
 int main() {
