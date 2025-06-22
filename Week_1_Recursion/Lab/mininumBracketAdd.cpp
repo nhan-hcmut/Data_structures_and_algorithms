fb81da412afe217cd5ce8a11338b02d359c8ba49
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-06-22 10:53:38 +0700

    Update mininumBracketAdd.cpp

--- Week_1_Recursion/Lab/mininumBracketAdd.cpp
+++ Week_1_Recursion/Lab/mininumBracketAdd.cpp
@@ -2,38 +2,34 @@
 using namespace std;
 
 // Helping functions
-int mininumBracketAddHelper(string &s, int index = 0, int unmatchedOpenBracket = 0, int unmatchedCloseBracket = 0) {
+int minimumBracketAddHelper(const string &s, int index = 0, int unmatchedOpenBracket = 0, int unmatchedCloseBracket = 0) {
+    if (index >= s.length()) return unmatchedOpenBracket + unmatchedCloseBracket; // If index >= s.length() -> out of bounds -> all parentheses are checked -> stop
 
-    if (s[index] == '(') return mininumBracketAddHelper(s, index+1, unmatchedOpenBracket+1, unmatchedCloseBracket); // if s[index] is an open bracket, there is one more unmatched one
+    if (s[index] == '(') return minimumBracketAddHelper(s, index + 1, unmatchedOpenBracket + 1, unmatchedCloseBracket); // if s[index] is an open bracket, there is one more unmatched one
 
-    if (s[index] == ')') {
-
-        if (unmatchedOpenBracket) return mininumBracketAddHelper(s, index+1, unmatchedOpenBracket-1, unmatchedCloseBracket); // if s[index] is a close bracket and there are some unmatched open brackets, we need to decrease the number of unmatched open brackets by 1
-
-        return mininumBracketAddHelper(s, index+1, unmatchedOpenBracket, unmatchedCloseBracket+1); // if s[index] is a close bracket and there is no unmatched open bracket, we need to increase the number of unmatched close bracket by 1
-    }
+    if (unmatchedOpenBracket) return minimumBracketAddHelper(s, index + 1, unmatchedOpenBracket - 1, unmatchedCloseBracket); // if s[index] is a close bracket and there are some unmatched open brackets, we need to decrease the number of unmatched open brackets by 1
     
-    return unmatchedOpenBracket + unmatchedCloseBracket;
+    return minimumBracketAddHelper(s, index + 1, unmatchedOpenBracket, unmatchedCloseBracket + 1); // if s[index] is a close bracket and there is no unmatched open bracket, we need to increase the number of unmatched close bracket by 1
 }
 // End helping functions
 
-int mininumBracketAdd(string s) {
-    return mininumBracketAddHelper(s);
+int minimumBracketAdd(string s) {
+    return minimumBracketAddHelper(s);
 }
 
 int main() {
     
-    cout << mininumBracketAdd(")))((") << endl; // result: 5
+    cout << minimumBracketAdd(")))((") << endl; // result: 5
 
-    cout << mininumBracketAdd("))()))()()") << endl; // result: 4
+    cout << minimumBracketAdd("))()))()()") << endl; // result: 4
 
-    cout << mininumBracketAdd("") << endl; // result: 0
+    cout << minimumBracketAdd("") << endl; // result: 0
 
-    cout << mininumBracketAdd(")()))()))())())))))(") << endl; // result: 12
+    cout << minimumBracketAdd(")()))()))())())))))(") << endl; // result: 12
 
-    cout << mininumBracketAdd(")())(((())()())()())))()()))(()((((()(()()()))()((())((()))))))()(((()(((())(((()()(())(())))))(()))") << endl; // result: 10
+    cout << minimumBracketAdd(")())(((())()())()())))()()))(()((((()(()()()))()((())((()))))))()(((()(((())(((()()(())(())))))(()))") << endl; // result: 10
     
-    cout << mininumBracketAdd(")(()())())))))(((())))))())))()))))))((())(((()(((())((()(()))())(()(()((((((())))(((()))))((())((()()))((())(()(((()())()()())(())())))(()))()))((((()()(()))()))))()()()(()))()))()())()(()())))())())()))())())((()()(())((()()(((()((()((((((((())()()())(()()()))()()()())()(()(())())((()))((((()((((((()((()))))))()))((()())))()()((((()()()(()())((((()((((((())))(()))))(()))))()())())(((())()())()(((())))(())(((()(((())))(((()()(()(((()((()(()))())))))()(()(())()())(((()(()(())()()())()(()()))(((()))()())()))))((()())(())()))(()))())(())))()))())()()(()()(((((()(()()(()()))(()(()(())()((((()((()(()(())))()())((()()(((()))(()))()(())()((()()(((((())(((())(()())()))))()((()))((()))(()())()))()())()))(()((()()()((()()))(())(())(((())()())(())()((())(((()(())(()))(())()(((())((()())((()))(((((()(())()()()()(()(()))(()()())())))))()))))()()))(())(((((((((()))(())))(()))(()))(()((()()(((()))))(()(())((((()))(()))))(()((()((()(((()))()))(()((())()))))(()))(()()))()((((()(((()()(()))()((()(((())") << endl; // result: 70
+    cout << minimumBracketAdd(")(()())())))))(((())))))())))()))))))((())(((()(((())((()(()))())(()(()((((((())))(((()))))((())((()()))((())(()(((()())()()())(())())))(()))()))((((()()(()))()))))()()()(()))()))()())()(()())))())())()))())())((()()(())((()()(((()((()((((((((())()()())(()()()))()()()())()(()(())())((()))((((()((((((()((()))))))()))((()())))()()((((()()()(()())((((()((((((())))(()))))(()))))()())())(((())()())()(((())))(())(((()(((())))(((()()(()(((()((()(()))())))))()(()(())()())(((()(()(())()()())()(()()))(((()))()())()))))((()())(())()))(()))())(())))()))())()()(()()(((((()(()()(()()))(()(()(())()((((()((()(()(())))()())((()()(((()))(()))()(())()((()()(((((())(((())(()())()))))()((()))((()))(()())()))()())()))(()((()()()((()()))(())(())(((())()())(())()((())(((()(())(()))(())()(((())((()())((()))(((((()(())()()()()(()(()))(()()())())))))()))))()()))(())(((((((((()))(())))(()))(()))(()((()()(((()))))(()(())((((()))(()))))(()((()((()(((()))()))(()((())()))))(()))(()()))()((((()(((()()(()))()((()(((())") << endl; // result: 70
     
     return 0;
 }
