c70ebdf36ae614fd491faa1fad05c4a5321c8eef
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-06-30 16:19:31 +0700

    Update reverseFactorial.cpp

--- Week_1_Recursion/Lab/reverseFactorial.cpp
+++ Week_1_Recursion/Lab/reverseFactorial.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 int factorial(int num) {
     
-    if (num < 0) return -1;
-    
-    if (num < 2) return 1;
+    if (num == 1) return 1;
     
     return num * factorial(num - 1);
 }
 
-int reverseFactorialRec(int n, int result = 1) {
+// Helping function
+int reverseFactorialRec(int n, int result = 1) { // 'result' is initially set to 1 and is incremented until factorial of result == n or exceeds n
 
-    if (factorial(result) > n) return -1;
+    if (factorial(result) > n) return -1; // If factorial of result exceeds n, return -1 (no valid result found)
     
-    if (factorial(result) == n) return result;
+    if (factorial(result) == n) return result; // If factorial of result equals n, return the result (this is the number whose factorial equals n)
     
-    return reverseFactorialRec(n, result + 1);
+    return reverseFactorialRec(n, result + 1); // If factorial of result is less than n, try the next number (increment result and call recursively)
 }
+// End helping function
 
 int reverseFactorial(int n) {
     // STUDENT ANSWER
     
-    if (n < 1) return -1;
+    if (n < 1) return -1; // If n is less than 1, return -1 because no factorial is less than 1
     
     return reverseFactorialRec(n);
 }
