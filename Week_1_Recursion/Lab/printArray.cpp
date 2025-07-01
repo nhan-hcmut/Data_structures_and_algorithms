4659dde8de574b5461b0a9e2ada65cffb5a9af85
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-07-01 08:40:50 +0700

    Update printArray.cpp

--- Week_1_Recursion/Lab/printArray.cpp
+++ Week_1_Recursion/Lab/printArray.cpp
@@ -10,10 +10,20 @@ void printArray(int n) {
 }
 
 int main() {
-    printArray(5);
+    printArray(5); // Result: 0, 1, 2, 3, 4, 5
     cout << endl;
     
-    printArray(10);
+    printArray(10); // Result: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     cout << endl;
+
+    printArray(11); // Result: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+    cout << endl;
+
+    printArray(22); // Result: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
+    cout << endl;
+
+    printArray(1); // Result: 0, 1
+    cout << endl;
+    
     return 0;
 }
