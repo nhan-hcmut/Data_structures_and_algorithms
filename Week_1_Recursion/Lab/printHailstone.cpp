132ad7b00f106670a5a44cef9d4a22a2a96c242f
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-07-01 08:32:42 +0700

    Update printHailstone.cpp

--- Week_1_Recursion/Lab/printHailstone.cpp
+++ Week_1_Recursion/Lab/printHailstone.cpp
@@ -23,6 +23,18 @@ int main() {
     printHailstone(11); // Result: 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
     cout << endl;
 
+    printHailstone(12); // Result: 12 6 3 10 5 16 8 4 2 1
+    cout << endl;
+
+    printHailstone(13); // Result: 13 40 20 10 5 16 8 4 2 1
+    cout << endl;
+
+    printHailstone(22); // Result: 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+    cout << endl;
+
+    printHailstone(23); // Result: 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1
+    cout << endl;
+
     printHailstone(32); // Result: 32 16 8 4 2 1
     cout << endl;
 
