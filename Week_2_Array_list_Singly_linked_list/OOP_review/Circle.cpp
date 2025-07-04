bb756c6ed8b53ce70d3c89f5a4242ab6a5166695
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-07-04 11:45:20 +0700

    Update Circle.cpp

--- Week_2_Array_list_Singly_linked_list/OOP_review/Circle.cpp
+++ Week_2_Array_list_Singly_linked_list/OOP_review/Circle.cpp
@@ -18,7 +18,6 @@ class Point {
         this->x = 0;
         this->y = 0;
     }
-
     Point(double x, double y) {
         /*
          * STUDENT ANSWER
@@ -26,41 +25,36 @@ class Point {
         this->x = x;
         this->y = y;
     }
-
     void setX(double x) {
         /*
          * STUDENT ANSWER
         */
         this->x = x;
     }
-
     void setY(double y) {
         /*
          * STUDENT ANSWER
         */
         this->y = y;
     }
-
     double getX() const {
         /*
          * STUDENT ANSWER
         */
         return this->x;
     }
-
     double getY() const {
         /*
          * STUDENT ANSWER
         */
         return this->y;
     }
-
     double distanceToPoint(const Point& pointA) {
         /*
          * STUDENT ANSWER
          * TODO: calculate the distance from this point to point A in the coordinate plane
         */
-        return sqrt(pow(this->x - pointA.getX(), 2) + pow(this->y - pointA.getY(), 2));
+        return sqrt(pow(this->x - pointA.x, 2) + pow(this->y - pointA.y, 2));
     }
 };
 
