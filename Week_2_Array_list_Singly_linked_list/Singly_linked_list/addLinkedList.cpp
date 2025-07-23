a819a8252d835cbd3cbd31240106389345958034
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-07-23 10:13:15 +0700

    Update addLinkedList.cpp

--- Week_2_Array_list_Singly_linked_list/Singly_linked_list/addLinkedList.cpp
+++ Week_2_Array_list_Singly_linked_list/Singly_linked_list/addLinkedList.cpp
@@ -19,71 +19,129 @@ class LLNode {
     public:
     int val;
     LLNode* next;
+
     LLNode() : val(0), next(nullptr) {}
     LLNode(int val, LLNode* next) {
         this->val = val;
         this->next = next;
     }
-    // some more functions
+    // Helping methods
     static LLNode* createWithIterators(int*, int*);
     static void printList(LLNode*);
     void clear();
-    // End some more functions
+    // End helping methods
 };
 
 LLNode* addLinkedList(LLNode* l0, LLNode* l1) {
     // STUDENT ANSWER
-    LLNode *sum = new LLNode(0, nullptr), *cur = sum;
-    vector<int> result;
-    int plus = 0, remainder = 0;
-    while (l0 && l1) {
-        plus = l0->val + l1->val + remainder;
-        result.push_back(plus % 10);
-        if (plus >= 10) remainder = 1;
-        else remainder = 0;
+    LLNode* newHead = new LLNode(); // A dummy node which needs to be deleted at the end
+    LLNode* current = newHead;
+    int sum = 0, carry = 0;
+    
+    while (l0 != nullptr && l1 != nullptr) {
+        sum = l0->val + l1->val + carry;
+        carry = sum / 10;
+        sum %= 10;
+        current->next = new LLNode(sum, nullptr);
+        current = current->next;
         l0 = l0->next;
         l1 = l1->next;
     }
-    while (l0) {
-        plus = l0->val + remainder;
-        result.push_back(plus % 10);
-        if (plus >= 10) remainder = 1;
-        else remainder = 0;
+    while (l0 != nullptr) {
+        sum = l0->val + carry;
+        carry = sum / 10;
+        sum %= 10;
+        current->next = new LLNode(sum, nullptr);
+        current = current->next;
         l0 = l0->next;
     }
-    while (l1) {
-        plus = l1->val + remainder;
-        result.push_back(plus % 10);
-        if (plus >= 10) remainder = 1;
-        else remainder = 0;
+    while (l1 != nullptr) {
+        sum = l1->val + carry;
+        carry = sum / 10;
+        sum %= 10;
+        current->next = new LLNode(sum, nullptr);
+        current = current->next;
         l1 = l1->next;
     }
-    if (remainder) result.push_back(1);
-    for (unsigned long long i = 0; i < (unsigned long long)result.size(); i++) {
-        cur->next = new LLNode(result.at(i), nullptr);
-        cur = cur->next;
+    if (carry == 1) {
+        current->next = new LLNode(1, nullptr);
     }
-    cur = sum;
-    sum = sum->next;
-    delete cur;
-    return sum;
+    current = newHead;
+    newHead = newHead->next;
+    delete current;
+    return newHead;
 }
 
-// some more functions
-LLNode* LLNode::createWithIterators(int* start, int* end) {}
-void LLNode::printList(LLNode* head) {}
-void LLNode::clear() {}
-// End some more functions
+// Helping methods
+LLNode* LLNode::createWithIterators(int* start, int* end) {
+    if (start == nullptr) return nullptr;
+    
+    LLNode* head = new LLNode(*start, nullptr);
+    LLNode* current = head;
+    start++;
+    
+    while (start != end) {
+        current->next = new LLNode(*start, nullptr);
+        current = current->next;
+        start++;
+    }
+    return head;
+}
+
+void LLNode::printList(LLNode* head) {
+    cout << "\n[";
+    
+    if (head != nullptr) {
+        cout << head->val;
+        head = head->next;
+    }
+    while (head != nullptr) {
+        cout << ", " << head->val;
+        head = head->next;
+    }
+    cout << "]\n";
+}
+
+void LLNode::clear() {
+    LLNode* current = this;
+    LLNode* following = nullptr;
+    
+    while (current != nullptr) {
+        following = current->next;
+        delete current;
+        current = following;
+    }
+}
+// End helping methods
 
 int main() {
+    // test 1
     int arr1[] = {2, 9};
     int arr2[] = {0, 5};
+    
     LLNode* head1 = LLNode::createWithIterators(arr1, arr1 + sizeof(arr1) / sizeof(int));
     LLNode* head2 = LLNode::createWithIterators(arr2, arr2 + sizeof(arr2) / sizeof(int));
+    
     LLNode* newhead = addLinkedList(head1, head2);
     LLNode::printList(newhead); //result: [2, 4, 1]
+    
     head1->clear();
     head2->clear();
     newhead->clear();
+    
+    /* test 2
+    int arr1[] = {2, 3};
+    int arr2[] = {1, 8};
+    
+    LLNode* head1 = LLNode::createWithIterators(arr1, arr1 + sizeof(arr1) / sizeof(int));
+    LLNode* head2 = LLNode::createWithIterators(arr2, arr2 + sizeof(arr2) / sizeof(int));
+    
+    LLNode* newhead = addLinkedList(head1, head2);
+    LLNode::printList(newhead); // Result: [3, 1, 1]
+    
+    head1->clear();
+    head2->clear();
+    newhead->clear();
+    */
     return 0;
 }
