46653cbac02c5098e5d19208aa217391bc665042
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-09-05 17:06:00 +0700

    Add files via upload

---
+++ Week_2_Array_list_Singly_linked_list/Singly_linked_list/replaceFirstGreater.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class LLNode {
+public:
+    int val;
+    LLNode* next;
+
+    LLNode() : val(0), next(nullptr) {}; // Constructor: val = 0, next = nullptr
+    LLNode(int val, LLNode* next) : val(val), next(next) {}; // Constructor with customized data
+
+    // Helping methods
+    static LLNode* createWithIterators(int* start, int* end) {
+        if (start == nullptr) return nullptr;
+        LLNode* head = new LLNode(*start, nullptr);
+        LLNode* current = head;
+        start++;
+        while (start != end) {
+            current->next = new LLNode(*start, nullptr);
+            current = current->next;
+            start++;
+        }
+        return head;
+    }
+    void printList() const {
+        cout << "[" << this->val;
+        LLNode* temp = this->next;
+        while (temp != nullptr) {
+            cout << "," << temp->val;
+            temp = temp->next;
+        }
+        cout << "]" << endl;
+    }
+    void clear() {
+        while (this->next != nullptr) {
+            LLNode *prev = this, *curr = this->next;
+            while (curr->next != nullptr) {
+                prev = curr;
+                curr = curr->next;
+            }
+            prev->next = nullptr;
+            delete curr;
+        }
+        delete this;
+    }
+};
+
+/* TODO */
+LLNode* replaceFirstGreater(LLNode* head) {
+    // STUDENT ANSWER
+    LLNode* curr = head;
+
+    while (curr->next != nullptr) {
+        LLNode* temp = curr->next;
+        int max = curr->val;
+
+        while (temp != nullptr) {
+            if (temp->val > max) max = temp->val;
+            temp = temp->next;
+        }
+        if (max > curr->val) curr->val = max;
+        else curr->val = 0;
+
+        curr = curr->next;
+    }
+    curr->val = 0;
+    return head;
+}
+
+int main() {
+
+    // Test 1
+    int arr1[] = {6};
+    LLNode* head1 = LLNode::createWithIterators(arr1, arr1 + sizeof(arr1) / sizeof(int));
+    head1->printList(); // result: [6]
+    head1 = replaceFirstGreater(head1);
+    head1->printList(); // result: [0]
+    head1->clear();
+
+    // Test 2
+    int arr2[] = {1, 3};
+    LLNode* head2 = LLNode::createWithIterators(arr2, arr2 + sizeof(arr2) / sizeof(int));
+    head2->printList(); // result: [1,3]
+    head2 = replaceFirstGreater(head2);
+    head2->printList(); // result: [3,0]
+    head2->clear();
+
+    // Test 3
+    int arr3[] = {3, 1, 4};
+    LLNode* head3 = LLNode::createWithIterators(arr3, arr3 + sizeof(arr3) / sizeof(int));
+    head3->printList(); // result: [3,1,4]
+    head3 = replaceFirstGreater(head3);
+    head3->printList(); // result: [4,4,0]
+    head3->clear();
+
+    // Test 4
+    int arr4[] = {5, 7, 1, 4};
+    LLNode* head4 = LLNode::createWithIterators(arr4, arr4 + sizeof(arr4) / sizeof(int));
+    head4->printList(); // result: [5,7,1,4]
+    head4 = replaceFirstGreater(head4);
+    head4->printList(); // result: [7,0,4,0]
+    head4->clear();
+    
+    // Test 5
+    int arr5[] = {5, 8, 8, 2, 3};
+    LLNode* head5 = LLNode::createWithIterators(arr5, arr5 + sizeof(arr5) / sizeof(int));
+    head5->printList(); // result: [5,8,8,2,3]
+    head5 = replaceFirstGreater(head5);
+    head5->printList(); // result: [8,0,0,3,0]
+    head5->clear();
+    
+    return 0;
+}
